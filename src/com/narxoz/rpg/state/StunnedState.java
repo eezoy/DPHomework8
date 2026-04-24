2fd0370cb516ae221e3063a55c83394182cd8e5c
eezoy <kulegor4ik@gmail.com>
2026-04-24 18:44:14 +0500

    Phase 3. Created fourth concrete state implementation StunnedState (4)

---
+++ src/com/narxoz/rpg/state/StunnedState.java
@@ -0,0 +1,46 @@
+﻿package com.narxoz.rpg.state;
+
+import com.narxoz.rpg.combatant.Hero;
+
+public class StunnedState implements HeroState {
+
+    private int turnsRemaining;
+
+    public StunnedState() {
+        this(1);
+    }
+
+    public StunnedState(int turnsRemaining) {
+        this.turnsRemaining = turnsRemaining;
+    }
+
+    @Override
+    public String getName() { return "Stunned"; }
+
+    @Override
+    public int modifyOutgoingDamage(int basePower) {
+        return 0;
+    }
+
+    @Override
+    public int modifyIncomingDamage(int rawDamage) {
+        return (int)(rawDamage * 1.5);
+    }
+
+    @Override
+    public void onTurnStart(Hero hero) {
+        System.out.println(hero.getName() + " is stunned and skips their turn.");
+    }
+
+    @Override
+    public void onTurnEnd(Hero hero) {
+        turnsRemaining--;
+        if (turnsRemaining <= 0) {
+            System.out.println(hero.getName() + " shakes off the stun.");
+            hero.setState(new NormalState());
+        }
+    }
+
+    @Override
+    public boolean canAct() { return false; }
+}
