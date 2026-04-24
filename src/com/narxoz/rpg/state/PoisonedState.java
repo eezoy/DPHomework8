330f606448f42ef94bae63f085d6bf166e139405
eezoy <kulegor4ik@gmail.com>
2026-04-24 18:16:25 +0500

    Phase 3. Created third concrete state implementation PoisonedState (3)

---
+++ src/com/narxoz/rpg/state/PoisonedState.java
@@ -0,0 +1,49 @@
+﻿package com.narxoz.rpg.state;
+
+import com.narxoz.rpg.combatant.Hero;
+
+public class PoisonedState implements HeroState {
+
+    private int turnsRemaining;
+
+    public PoisonedState() {
+        this(3);
+    }
+
+    public PoisonedState(int turnsRemaining) {
+        this.turnsRemaining = turnsRemaining;
+    }
+
+    @Override
+    public String getName() { return "Poisoned"; }
+
+    @Override
+    public int modifyOutgoingDamage(int basePower) {
+        return (int)(basePower * 0.7);
+    }
+
+    @Override
+    public int modifyIncomingDamage(int rawDamage) {
+        return rawDamage;
+    }
+
+    @Override
+    public void onTurnStart(Hero hero) {
+        System.out.println(hero.getName() + " takes 5 damage from poison.");
+        hero.takeDirect(5);
+    }
+
+    @Override
+    public void onTurnEnd(Hero hero) {
+        turnsRemaining--;
+        if (turnsRemaining <= 0) {
+            System.out.println(hero.getName() + " is no longer poisoned.");
+            hero.setState(new NormalState());
+        } else {
+            System.out.println(hero.getName() + " is still poisoned (" + turnsRemaining + " turns left).");
+        }
+    }
+
+    @Override
+    public boolean canAct() { return true; }
+}
